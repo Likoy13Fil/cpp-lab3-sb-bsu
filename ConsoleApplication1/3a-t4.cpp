78bb4121bee55ac09df14e00514460c27c60b414
Likoy13Fil <fil.avchinnikov@gmail.com>
2024-10-23 20:52:31 +0300

    Update 3a-t4.cpp

--- ConsoleApplication1/3a-t4.cpp
+++ ConsoleApplication1/3a-t4.cpp
@@ -3,13 +3,26 @@ using namespace std;
 int main()
 {
 	int k, i = 1, sum_k = 0, number;
-	cout << " Enter the amount of integers to sum = "; cin >> k;
-	do {
+	while (true)
+	{
+		cout << " Enter the amount of integers to sum = "; cin >> k;
+		if (k <= 0) {
+			cout << "error, please, input corrert value" << endl;
+			cin.clear();
+			cin.ignore(100, '\n');
+			continue;
+		}
+		break;
+	}
+	do
+	{
 		cout << "Enter integer nr. " << i << ": ";
 		cin >> number;
+		if (!cin) { cout << "error, please, input corrert value" << endl; cin.clear(); cin.ignore(10000, '\n'); continue; }
 		sum_k += number; i++;
 	} while (i <= k);
 	cout << " The total sum of " << k << " integers is: " << sum_k;
 	return 0;
 }
+
 
